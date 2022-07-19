125d39bce0481b216e128027b1eb3f030b4f698c
elizabethvera-atieno <atienolizvera@gmail.com>
2022-07-19 15:10:29 +0300

    commit tree 7708f9f60e12e715675be2ddec736b6b189609d2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+                 Student.find_by(first_name: params[:name].downcase.titleize) || Student.find_by(last_name: params[:name].downcase.titleize)
+               else
+                 Student.all
+               end    
+    render json: students
+  end
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
